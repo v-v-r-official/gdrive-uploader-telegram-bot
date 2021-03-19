c81cfac91f9b4758d130b008f59467eb2165b4ff
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-19 19:07:33 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -41,8 +41,8 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
 
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url = "https://www.github.com/cdfxscrq")],
-            [InlineKeyboardButton(text = 'Feature Request', url = "https://t.me/Akshayan1")],
+            [InlineKeyboardButton(text = 'Channel', url = "https://t.me/VKPROJECTS")],
+            [InlineKeyboardButton(text = 'Group', url = "https://t.me/VKP_BOTS")],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
         ]
